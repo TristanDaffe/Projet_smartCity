8684e61990807d52e659fca94ec48d69dcb12bc8
Tristan D <etu45639@henallux.be>
2022-11-13 14:56:52 +0100

    Top bar
    le nom de la topbar avec fleche est changée en TopBarArrow
    le nom des fonctions de top bar est corrigé

--- src/components/topBarDrawer.jsx
+++ src/components/topBarDrawer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, View, StyleSheet, Image, Pressable } from 'react-native';
 
-export default function hamburgerMenu (props) {   
+export default function TopBarDrawer (props) {   
     return (
         <View style={styles.container}>
             <View styles={styles.menu}>
