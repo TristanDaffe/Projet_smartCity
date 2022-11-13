8cc3ec646cc2b38811ee4ada43233bf6ac6dee99
Tristan D <etu45639@henallux.be>
2022-11-13 16:36:20 +0100

    Fichier test perso
    fichier pour faire des test à afficher dans Home

---
+++ src/screens/Test/TestTris.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import {  View, Text, StyleSheet  } from 'react-native';
+
+
+export default function TestTris ( {navigation} )  {   
+    return (
+      <View>
+        <Text> Bouh </Text>
+      </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
