18b646fe2cb6c6920d1453b43c3b99cea7ce8538
BenjaminRousseau37416 <etu37416@henallux.be>
2022-11-22 16:18:58 +0100

    Début des scripts

--- API/scripts/SQL/createDB.sql
+++ API/scripts/SQL/createDB.sql
@@ -62,7 +62,7 @@ DROP TABLE IF EXISTS donation_type;
 CREATE TABLE donation_type(
     id int AUTO_INCREMENT primary key,
     name varchar(50) not null unique,
-    time_of_donation date not null,
+    time_of_donation time not null,
 );
 
 DROP TABLE IF EXISTS time_between_donation;
@@ -71,7 +71,7 @@ CREATE TABLE time_between_donation(
         foreign key (first_donation_type_id) references donation_type(id),
     next_donation_type_id int not null,
         foreign key (next_donation_type_id) references donation_type(id),
-    interval time not null,
+    interval date not null,
     primary key (first_donation_type_id,next_donation_type_id)
 );
 
@@ -104,4 +104,76 @@ CREATE TABLE donation(
         + (select interval from time_between_donation 
         where  next_donation_type_id = donation_type_id and first_donation_type_id = (select donation_type_id from donation where user_id = user_id order by date desc limit 1)))
 );
+
+/* Insert */
+INSERT INTO opening_day(day_label, opening_time, closing_time) VALUES
+('Monday', '08:00:00', '12:00:00'),
+('Tuesday', '08:00:00', '12:00:00'),
+('Wednesday', '08:00:00', '12:00:00'),
+('Thursday', '08:00:00', '12:00:00'),
+('Friday', '08:00:00', '12:00:00'),
+('Saturday', '08:00:00', '12:00:00'),
+('Sunday', '08:00:00', '12:00:00');
+
+INSERT INTO blood_type(type, rhesus) VALUES
+('A', '+'),
+('A', '-'),
+('B', '+'),
+('B', '-'),
+('AB', '+'),
+('AB', '-'),
+('O', '+'),
+('O', '-');
+
+INSERT INTO donation_type(name, time_of_donation) VALUES
+('Blood', '00:20:00'),
+('Plasma', '00:45:00'),
+('Platelets', '01:30:00');
+
+INSERT INTO time_between_donation(first_donation_type_id, next_donation_type_id, interval) VALUES
+(1, 1, '00-03-00'),
+(1, 2, '00-00-14'),
+(1, 3, '00-00-28'),
+(2, 1, '00-00-14'),
+(2, 2, '00-00-14'),
+(2, 3, '00-00-14'),
+(3, 1, '00-00-28'),
+(3, 2, '00-00-14'),
+(3, 3, '00-00-28');
+
+INSERT INTO locality(name, postal_code) VALUES
+('Namur', 5000),
+('Belgrade', 5001),
+('Champion', 5020),
+('Jambes', 5100),
+('Jemeppe-sur-Sambre', 5190),
+('Sambreville', 5060),
+('Gembloux', 5030);
+
+INSERT INTO user_account(first_name, last_name, birthday, email_address, login, password, blood_type) VALUES
+('John', 'Doe', '1990-01-01', 'john.doe@gmail.com', johnDoeLeBoss, '1234', 1),
+('Jane', 'Doe', '1990-01-01', 'jane.doe@gmail.com', janeDoeLaBoss, '1234', 2),
+('John', 'Smith', '1990-01-01', 'john.smith@gmail.com', johnSmithLeBoss, '1234', 3);
+
+Insert into donation_center(name, phone_number, email_address, fax, street_name, street_number, locality) VALUES
+('Namur', '04/123.45.67', 'caca@caca.lol', '+32 4 123 45 67', 'Rue de la Paix', 1, 1);
+
+Insert into open_day(center_id, day_id) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 6),
+(1, 7);
+
+Insert into donation_available(center_id, blood_type_id) VALUES
+(1, 1),
+(1, 2),
+(1, 3);
+
+Insert into donation(date, user_id, donation_type_id, donation_center_id) VALUES
+('2019-01-01', 1, 1, 1),
+('2019-01-01', 2, 1, 1),
+('2019-01-01', 3, 1, 1);
 
