5a6bc128bb41b0d0cfe2c77354085ef07c24faa1
BenjaminRousseau37416 <etu37416@henallux.be>
2022-11-23 18:58:54 +0100

    Scripts utilisables

--- API/scripts/SQL/createDB.sql
+++ API/scripts/SQL/createDB.sql
@@ -107,7 +107,14 @@ INSERT INTO opening_day(day_label, opening_time, closing_time) VALUES
 ('Thursday', '08:00:00', '12:00:00'),
 ('Friday', '08:00:00', '12:00:00'),
 ('Saturday', '08:00:00', '12:00:00'),
-('Sunday', '08:00:00', '12:00:00');
+('Sunday', '08:00:00', '12:00:00'),
+('Monday', '13:00:00', '18:00:00'),
+('Tuesday', '13:00:00', '18:00:00'),
+('Wednesday', '13:00:00', '15:00:00'),
+('Thursday', '13:00:00', '16:30:00'),
+('Friday', '13:00:00', '16:30:00'),
+('Saturday', '13:00:00', '20:00:00'),
+('Sunday', '13:00:00', '17:00:00');
 
 INSERT INTO blood_type(type, rhesus) VALUES
 ('A', '+'),
@@ -135,22 +142,42 @@ INSERT INTO time_between_donation(first_donation_type_id, next_donation_type_id,
 (3, 2, '2 week'),
 (3, 3, '3 week');
 
+/* TODO */
 INSERT INTO locality(name, postal_code) VALUES
 ('Namur', 5000),
+('Saint-Servais', 5002),
 ('Belgrade', 5001),
 ('Champion', 5020),
 ('Jambes', 5100),
 ('Jemeppe-sur-Sambre', 5190),
 ('Sambreville', 5060),
+('Yvoir', 5530),
 ('Gembloux', 5030);
+('Namur', 5020);
+('Charleroi', 6000);
+('Marche-en-Famenne', 6900);
 
+/* TODO */
 INSERT INTO user_account(first_name, last_name, birthday, email_address, login, password, blood_type) VALUES
-('John', 'Doe', '1990-01-01', 'john.doe@gmail.com', 'johnDoeLeBoss', '1234', 1),
-('Jane', 'Doe', '1990-01-01', 'jane.doe@gmail.com', 'janeDoeLaBoss', '1234', 2),
-('John', 'Smith', '1990-01-01', 'john.smith@gmail.com', 'johnSmithLeBoss', '1234', 3);
+('John', 'Doe', '1990-01-01', 'john.doe@gmail.com', 'johnDoe90', '1234', 1),
+('Jane', 'Doe', '1991-10-10', 'jane.doe@gmail.com', 'janeDoeLaBoss91', '1234', 2),
+('John', 'Smith', '1992-12-31', 'john.smith@gmail.com', 'johnSmithLeBoss', '1234', 3),
+('Jane', 'Smith', '1993-01-01', 'jane.smith@skynet.be', 'janeSmith93', '1234', 4),
+('Jean', 'Dupuis', '1994-02-02', 'john.dupuis@skynet.be', 'JD94', '1234', 5),
+('François', 'Legrand', '1995-03-03', 'francois.legrand@hotmail.com', 'FL95', '1234', 6),
+('Sarah', 'Lemaitre', '1996-04-04', 'sarah.lemaitre@hotmail.com', 'SL96', '1234', 7),
+('Marie', 'Leroy', '1997-05-05', 'marie.leroy@hotmail.com', 'ML97', '1234', 8);
 
+
+-- En fait y'a pas toujours de numéro de fax mais j'en ai mis pour remplir
+-- J'ai mis des vrais
 Insert into donation_center(name, phone_number, email_address, fax, street_name, street_number, locality) VALUES
-('Namur', '0495/123.45.67', 'caca@caca.lol', '+32 4 123 45 67', 'Rue de la Paix', 1, 1);
+('Don de sang Croix-Rouge Namur', '081/22.10.10', 'info@croix-rouge.be', '0800 92 245', 'Rue des Dames Blanches', 34, 1);
+('Don de sang - Établissement de transfusion sanguine', '081/42.21.35', null , '0800 92 245', 'Rue Dr Gaston Therasse', 1, 8);
+('La Transfusion du Sang', '071/53.29.99', null , '0800 92 245', 'Bd Joseph II', 11, 9);
+('Don de sang à Marche-en-Famenne', '081/56.41.52', null , '0800 92 245', 'Rue du Vivier', 24, 10);
+-- Celui-là il a même pas de numéro de tel
+('prélèvement prise de sang spy', null, null , '0800 92 245', 'Rte de Saussin', 1, 6);
 
 Insert into open_day(center_id, day_id) VALUES
 (1, 1),
@@ -159,15 +186,56 @@ Insert into open_day(center_id, day_id) VALUES
 (1, 4),
 (1, 5),
 (1, 6),
-(1, 7);
+(1, 7),
+(2, 1),
+(2, 2),
+(2, 3),
+(2, 4),
+(2, 5),
+(2, 6),
+(2, 7),
+(3, 1),
+(3, 2),
+(3, 3),
+(3, 4),
+(3, 5),
+(3, 6),
+(3, 7),
+(4, 1),
+(4, 2),
+(4, 3),
+(4, 4),
+(4, 5),
+(4, 6),
+(4, 7),
+(5, 1),
+(5, 2),
+(5, 3),
+(5, 4),
+(5, 5),
+(5, 6),
+(5, 7);
 
 Insert into donation_available(center_id, blood_type_id) VALUES
 (1, 1),
 (1, 2),
 (1, 3);
+(2, 1);
+(3, 1);
+(3, 2);
+(3, 3);
+(4, 1);
+(4, 2);
+(4, 3);
+(5, 1);
 
 Insert into donation(date, user_id, donation_type_id, donation_center_id) VALUES
 ('2019-01-01', 1, 1, 1),
-('2019-01-01', 2, 1, 1),
-('2019-01-01', 3, 1, 1);
+('2020-03-24', 2, 1, 1),
+('2022-06-15', 3, 1, 1),
+('2019-11-21', 4, 1, 1),
+('2020-02-03', 5, 1, 1),
+('2022-11-23', 6, 1, 1),
+('2021-09-09', 7, 1, 1),
+('2022-04-12', 8, 1, 1);
 
