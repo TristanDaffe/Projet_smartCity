76c8e5b92b7a872a9419dacffd0428126cb24f0e
Tristan D <etu45639@henallux.be>
2022-12-01 18:32:16 +0100

    API
    .env à créé pour les infos importante
    nouveau package

--- API/model/database.js
+++ API/model/database.js
@@ -1,12 +1,15 @@
+require("dotenv").config();
+const process = require("process");
+
 const pg = require('pg');
 const Pool = pg.Pool;
 
 const pool = new Pool({
-    user: 'admin',
-    host: 'localhost',
-    database: 'smartcity',
-    password: 'password',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 module.exports = pool;
