7955d670fd3ab8ef01d81871782acf0da3cf2bdb
Alucarrdk <etu46691@henallux.be>
2022-12-19 15:43:58 +0100

    Modification :
     - Récupère le type et center sur les pages
    
    Test :
    - page modifier pour format téléphone ( à tester )
    
    Problème actuel :
    - retard au niveau de l'affectation de la variable

--- Mobile/screens/type.jsx
+++ Mobile/screens/type.jsx
@@ -1,26 +1,55 @@
 import React, { useState } from "react";
-import {  View, Text, StyleSheet,ScrollView,SafeAreaView , Button} from 'react-native';
+import {  View, Text, StyleSheet,SafeAreaView , Button} from 'react-native';
 
-import TopBarDrawer from '../components/topBar/topBarDrawer';
+import TopBar from '../components/topBar/topBarArrow';
 import MultiButton from '../components/MultiButton';
+import { ScrollView } from "react-native-gesture-handler";
 
-export default function Type ( {navigation} )  {   
+export default function Type ( {navigation} )  {  
+    
+    const [type, setType] = React.useState(null);
+    const [appointment, setAppointment] = React.useState({type : "" , center:""});
+
+    const returnType = (newType) => {
+      setAppointment(() => ({
+        type: newType
+     }));
+    }
+
     return (
-      <SafeAreaView>
-        <TopBarDrawer onclick={navigation.toggleDrawer}/>
-        <MultiButton style={{alignSelf: 'center'}}></MultiButton>
-        <View style={styles.button}>
-        <Button 
-          title='Next' 
-          color='red'
-          style={styles.button} 
-          onPress={() => navigation.navigate('Center')}></Button>
-        </View> 
-      </SafeAreaView>
+      <View>
+        <TopBar onclick={navigation.goBack}/>
+          <ScrollView horizontal={false}>
+            <Text style={styles.type}>Type</Text>
+              <ScrollView horizontal={true}>
+              <MultiButton returnType = {returnType}></MultiButton>
+              </ScrollView>
+              <View style={styles.button}>
+              <Button 
+                title='Next' 
+                color='red'
+                style={styles.button} 
+                onPress={() => navigation.navigate('Center', appointment)
+                  }> 
+                </Button>
+              </View> 
+          </ScrollView>
+      </View>
     );
 };
 
 const styles = StyleSheet.create({
+    container:
+    {
+      maxHeight: '100%',
+    },
+    type: {
+      fontWeight: 'bold',
+      fontSize: 30,
+      marginTop: 50,
+      marginLeft: 20,
+      marginBottom: 20,
+    },
     textName: {
       alignSelf: 'center',
       fontWeight: 'bold',
@@ -37,7 +66,7 @@ const styles = StyleSheet.create({
         width: "90%",
         marginTop: 30,
         marginLeft: 20,
-        height: 300,
+        height: 200,
       },
     textResponseBlood: {
       fontWeight: 'bold',
