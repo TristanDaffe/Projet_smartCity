0746b849778b9c7acb5b23aed349b47c0399f039
Susu <etu45639@henallux.be>
2022-12-21 10:07:30 +0100

    API routes
    début des routes :
    - router de user
    
    reste :
    - controlleur user
    - composant de swagger
    - 2eme routes

--- API/route/user.js
+++ API/route/user.js
@@ -13,3 +13,150 @@ router.patch('/', JWTMiddleWare.identification, AuthMiddleWare.mustBeAdmin, User
 router.delete('/:id', JWTMiddleWare.identification, AuthMiddleWare.mustBeAdmin, UserConroller.deleteUser);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * /user/all:
+ * get:
+ *     tags:
+ *       - User
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         ref: "#/components/responses/AllUsers"
+ *       401:
+ *         description: Unauthorized
+ *       403:
+ *         description: Forbidden
+ *       500:
+ *         description: Internal Server Error
+*/
+
+/**
+ * @swagger
+ * /user/{id}:
+ * get:
+ *  tags:
+ *    - User
+ *  security:
+ *    - bearerAuth: []
+ *  parameters:
+ *    - name: id
+ *      description: id of the user
+ *      in: path
+ *      required: true
+ *      schema:
+ *       type: integer
+ *  responses:
+ *     200:
+ *      $ref: "#/components/responses/UserFound"
+ *     400:
+ *      description: Id is not a number
+ *     404:
+ *      description: User not found
+ *     401:
+ *      description: Unauthorized
+ *     403:
+ *      description: Forbidden
+ *     500:
+ *      description: Internal Server Error
+ * 
+*/
+
+/**
+ * @swagger
+ * /user/login:
+ * post:
+ *    tags:
+ *         - User
+ *    requestBody:
+ *         $ref :"#components/requestBodies/Login"
+ *    responses:
+ *     200:
+ *         $ref: "#/components/responses/Token"
+ *     404:
+ *         description: User not found
+ *     412:
+ *         description: Login or password is empty
+ *     500:
+ *         description: Internal Server Error
+*/
+
+/**
+ * @swagger
+ * /user/register:
+ * post:
+ *    tags:
+ *       - User
+ *    requestBody:
+ *       $ref: "#/components/requestBodies/Register"
+ *    responses:
+ *      200:
+ *       $ref: "#/components/responses/Token"
+ *      412:
+ *       description: A field is invalid
+ *      409:
+ *       description: Login or email already exists
+ *      404:
+ *       description: Blood type not found
+ *      500:
+ *       description: Internal Server Error
+*/
+
+/**
+ * @swagger
+ * /user:
+ * patch:
+ *  tags:
+ *       - User
+ *  security:
+ *       - bearerAuth: []
+ *  requestBody:
+ *       $ref: "#/components/requestBodies/Update"
+ *  responses:
+ *      200:
+ *       $ref: "#/components/responses/Token"
+ *      412:
+ *       description: A field is invalid
+ *      409:
+ *       description: Login or email already exists
+ *      404:
+ *       description: User not found
+ *      401:
+ *       description: Unauthorized
+ *      403:
+ *       description: Forbidden
+ *      500:
+ *       description: Internal Server Error
+*/
+
+/**
+ * @swagger
+ * /user/{id}:
+ * delete:
+ *  tags:
+ *      - User
+ *  security:
+ *      - bearerAuth: []
+ *  parameters:
+ *      - name: id
+ *        description: id of the user
+ *        in: path
+ *        required: true
+ *        schema:
+ *           type: integer
+ *  responses:
+ *      200:
+ *          description: User deleted
+ *      400:
+ *          description: Id is not a number
+ *      404:
+ *          description: User not found
+ *      401:
+ *          description: Unauthorized
+ *      403:
+ *          description: Forbidden
+ *      500:
+ *          description: Internal Server Error
+*/
